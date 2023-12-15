df5b25d613eb5e65f0aa852fbe74804b0527a0ef
SeongJeongKyu <juku1133@gmail.com>
2023-12-15 23:19:46 +0900

    test.py 삭제

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-server_addr = "http://10.223.113.129:8000"
-data = {"name" : "코리안섹스머신성정규", "student_id" : "201911113213121", "password" : "12",  "path": "/student/signin"}
-res = requests.post(server_addr, json = data, headers={"accept": "application/json","Content-Type": "application/json",})
-
-print(res.json()["status"])
